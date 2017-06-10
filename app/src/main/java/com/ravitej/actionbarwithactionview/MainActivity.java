ec16247e718a76b3a782563dbb83f666979fec43
ravi tej <k.ravitej4u@gmail.com>
2017-06-09 20:33:34 -0400

    Complete Preference Screen Changes

--- app/src/main/java/com/ravitej/actionbarwithactionview/MainActivity.java
+++ app/src/main/java/com/ravitej/actionbarwithactionview/MainActivity.java
@@ -1,16 +1,20 @@
 package com.ravitej.actionbarwithactionview;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.preference.PreferenceManager;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements
+        SharedPreferences.OnSharedPreferenceChangeListener{
+        TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +22,15 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar)findViewById(R.id.my_toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("My Activity");
+
+        textView = (TextView)findViewById(R.id.tv_maintext);
+        setSharedPreferences();
+    }
+
+    private void setSharedPreferences() {
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+        textView.setText("Hello "+ sharedPref.getString("user_name","User!!!"));
+        sharedPref.registerOnSharedPreferenceChangeListener(this);
     }
 
     @Override
@@ -70,5 +83,18 @@ public class MainActivity extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
 
         }
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        if(key.equals("user_name")){
+            textView.setText("Hello " + sharedPreferences.getString(key,"User")+"!!!");
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        PreferenceManager.getDefaultSharedPreferences(this).unregisterOnSharedPreferenceChangeListener(this);
     }
 }
--- app/src/main/java/com/ravitej/actionbarwithactionview/SettingsPreferenceScreen.java
+++ app/src/main/java/com/ravitej/actionbarwithactionview/SettingsPreferenceScreen.java
@@ -4,6 +4,7 @@ import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
 import android.support.v7.preference.CheckBoxPreference;
+import android.support.v7.preference.EditTextPreference;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
@@ -40,6 +41,8 @@ public class SettingsPreferenceScreen extends PreferenceFragmentCompat implement
             if(valueIndex >= 0){
                 list.setSummary(list.getEntries()[valueIndex]);
             }
+        }else if(p instanceof EditTextPreference){
+            p.setSummary(value);
         }
     }
 
